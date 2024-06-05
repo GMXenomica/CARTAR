fe1d0202409e73393518daf5ae6dd8abbc38052c
GMX <163541065+GMXenomica@users.noreply.github.com>
2024-06-05 14:32:43 -0700

    Update Help.py

--- Help.py
+++ Help.py
@@ -7,51 +7,7 @@ st.set_page_config(
     layout='wide'
 )
 
-@st.cache_data
-def get_base64_of_bin_file(png_file):
-    with open(png_file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def build_markup_for_logo(
-    png_file,
-    background_position="50% 10%",
-    image_width="90%",
-    image_height="",
-):
-    binary_string = get_base64_of_bin_file(png_file)
-    return """
-            <style>
-                [data-testid="stSidebarNav"] {
-                    background-image: url("data:image/png;base64,%s");
-                    background-repeat: no-repeat;
-                    background-position: %s;
-                    background-size: %s %s;
-                }
-                [data-testid="stSidebarNav"]::before {
-                content: "";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 15px;
-                position: relative;
-                top: 100px;
-                }
-            </style>
-            """ % (
-        binary_string,
-        background_position,
-        image_width,
-        image_height,
-    )
-
-def add_logo(png_file):
-    logo_markup = build_markup_for_logo(png_file)
-    st.markdown(
-        logo_markup,
-        unsafe_allow_html=True,
-    )
-
-add_logo('logo_v2.png')
+st.logo('logo_v2.png', icon_image='logo.png')
 
 mystyle = '''
     <style>
