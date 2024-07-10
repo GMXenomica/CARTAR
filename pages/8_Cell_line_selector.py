563945ce23e53b0d0728d4b0231435b788cdbbc3
GMX <163541065+GMXenomica@users.noreply.github.com>
2024-07-10 13:44:40 -0700

    Update 8_Cell_line_selector.py

--- pages/8_Cell_line_selector.py
+++ pages/8_Cell_line_selector.py
@@ -28,6 +28,17 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+# Create footer
+def create_footer():
+    footer_container = st.container()
+    with footer_container:
+        st.markdown("<br>" * 1, unsafe_allow_html=True)  # Añade espacio en blanco
+        st.markdown("""
+        <div style="background-color: #f0f2f6; padding: 10px; text-align: center; font-size: 10.5px;">
+            How to cite: Miguel Hernandez-Gamarra, Alba Salgado-Roo, Eduardo Dominguez, Elena María Goiricelaya Seco, Sara Veiga-Rúa, Lucía F Pedrera-Garbayo, Ángel Carracedo, Catarina Allegue, CARTAR: a comprehensive web tool for identifying potential targets in chimeric antigen receptor therapies using TCGA and GTEx data, Briefings in Bioinformatics, Volume 25, Issue 4, July 2024, bbae326, <a href="https://doi.org/10.1093/bib/bbae326">https://doi.org/10.1093/bib/bbae326</a>.
+        </div>
+        """, unsafe_allow_html=True)
+
 st.markdown(mystyle, unsafe_allow_html=True)
 st.title('Cancer cell line selector')
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
@@ -171,9 +182,6 @@ if st.button('Find cell lines'):
             b64 = base64.b64encode(table.encode()).decode()
             href = f'<a href="data:file/csv;base64,{b64}" download="table.csv">Download CSV File</a>'
             st.markdown(href, unsafe_allow_html=True)
-            st.header('Cite us')
-            st.markdown('Please kindly cite us if you used this tool for your research: __Miguel Hernandez-Gamarra, Alba Salgado-Roo, Eduardo Dominguez, Elena María Goiricelaya Seco, Sara Veiga-Rúa, Lucía F Pedrera-Garbayo, Ángel Carracedo, Catarina Allegue, CARTAR: a comprehensive web tool for identifying potential targets in chimeric antigen receptor therapies using TCGA and GTEx data, Briefings in Bioinformatics, Volume 25, Issue 4, July 2024, bbae326, https://doi.org/10.1093/bib/bbae326.__')
-
         else:
             if expression == 'Underexpression':
                 st.error(f'No cell lines with {gene} expression under {value_d} {scale} have been found. Minimum value expression for {gene} in selected cell lines is {min_value} {scale}')
@@ -185,3 +193,4 @@ if st.button('Find cell lines'):
         st.error(f'{gene} gene symbol not found')
     if value == '':
         st.error('No threshold was introduced')
+create_footer()
